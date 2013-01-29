8b131b86cbc58284e50eeea93f872d2c0748c7a1
Nathan <ddragonsdd@gmail.com>
2013-01-28 22:01:32 -0600

    Work on tests

--- NETScoreTranscription/NETScoreTranscriptionLibrary/XMLStringFetcher.cs
+++ NETScoreTranscription/NETScoreTranscriptionLibrary/XMLStringFetcher.cs
@@ -6,22 +6,35 @@ namespace NETScoreTranscriptionLibrary
 {
     public class XMLStringFetcher
     {
-        public static string GetXMLFile(string file)
+        /// <summary>
+        /// Get the contents of a MusicXML file
+        /// </summary>
+        /// <param name="fileName">The name of the file to get the contents of</param>
+        /// <returns>A string of the contents of a MusicXML file</returns>
+        public static string GetXMLFile(string fileName)
         {
-            FileStream fs = null;
-            try
-            {
-                fs = new FileStream(AppDomain.CurrentDomain.BaseDirectory + "/MusicXMLSamples/" + file, FileMode.Open, FileAccess.Read);
-            }
-            catch(Exception e)
-            {
-                fs = new FileStream(AppDomain.CurrentDomain.BaseDirectory + "/MusicXMLTestSuite/" + file, FileMode.Open, FileAccess.Read);
-            }
+            FileStream fs = GetFileStream(fileName);
 
             StreamReader sr = new StreamReader(fs);
             String returnString = sr.ReadToEnd();
 
             return returnString;
+        }
+        /// <summary>
+        /// Get the FileStream for a faile
+        /// </summary>
+        /// <param name="fileName">The filename to get the stream for</param>
+        /// <returns>FileStream of fileName</returns>
+        public static FileStream GetFileStream(string fileName)
+        {
+            try
+            {
+                return new FileStream(AppDomain.CurrentDomain.BaseDirectory + "/MusicXMLSamples/" + fileName, FileMode.Open, FileAccess.Read);
+            }
+            catch (Exception e)
+            {
+                return new FileStream(AppDomain.CurrentDomain.BaseDirectory + "/MusicXMLTestSuite/" + fileName, FileMode.Open, FileAccess.Read);
+            }
         }
     }
 }
